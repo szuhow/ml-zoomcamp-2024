46eca9199135749d3593fb9bf19fcdf9b0c5d248
szuhow <magiedeszu@gmail.com>
2024-09-20 12:08:22 +0200

    rm: main

--- 1_Intro_to_ML/main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# print(pd.__version__)
-
-data = pd.read_csv('laptops.csv')
-# print(len(data))
-#  print number of types of laptops
-print(len(data['Brand'].value_counts()))
-print(data.isnull().sum())
